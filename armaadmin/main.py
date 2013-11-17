76d46beb011620d5a2d4374961aa6a6d44105af6
Foster McLane <fkmclane@gmail.com>
2013-11-17 16:09:05 -0500

    Gracefully quit on SIGTERM

--- armaadmin/main.py
+++ armaadmin/main.py
@@ -1,15 +1,24 @@
+import signal
 import time
 
 from armaadmin import interface, manager, web
+
+running = True
+
+def sigterm(signum, frame):
+	global running
+	running = False
+
+signal.signal(signal.SIGTERM, sigterm)
+
+web.init(interface.routes)
 
 for server in manager.servers.values():
 	if server.exists():
 		server.start()
 		time.sleep(3)
 
-web.init(interface.routes)
-
-while True:
+while running:
 	manager.poll()
 	time.sleep(3)
 
