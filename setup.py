25cf6a8a831be27c9102542047aab04f986846c3
Foster McLane <fkmclane@gmail.com>
2013-11-17 16:28:57 -0500

    Restructured setup file and added upgrade function

--- setup.py
+++ setup.py
@@ -8,59 +8,78 @@ import os
 import shutil
 import subprocess
 
+def setupUser():
+	import armaadmin.users
+
+	print()
+	print('Please set up the administrator account.')
+
+	username = input('Username: ')
+	password = getpass.getpass('Password: ')
+
+	armaadmin.users.add(username, password, [], True)
+
+def setupDirs():
+	import config
+
+	print()
+	print('Making directories...')
+
+	dir_util.mkpath(config.prefix)
+
+	if config.creation:
+		dir_util.copy_tree('config', config.config)
+
+	if config.api:
+		print()
+		print('Installing API...')
+		dir_util.copy_tree('api', config.api)
+
+def setupInit():
+	response = input('Which init system are you using: [1] SysV (Debian, Ubuntu, CentOS), [2] OpenRC (Gentoo), [3] Systemd (Arch, Fedora), [*] Other/None? ')
+
+	if response == "1":
+		print()
+		print('Installing SysV init script...')
+		file_util.copy_file('init/sysv/armaadmin', '/etc/init.d/')
+	elif response == "2":
+		print()
+		print('Installing OpenRC init script...')
+		file_util.copy_file('init/openrc/armaadmin', '/etc/init.d/')
+	elif response == "3":
+		print()
+		print('Installing Systemd init script...')
+		file_util.copy_file('init/systemd/armaadmin.service', '/usr/lib/systemd/system/')
+		subprocess.call(['systemctl', 'daemon-reload'])
+
 class post_install(install):
 	def run(self):
 		open('armaadmin/users.db', 'w').close()
-		shutil.copy('config.py', 'armaadmin/')
-
-		import armaadmin.users
-
-		print()
-		print('Please set up the administrator account.')
-
-		username = input('Username: ')
-		password = getpass.getpass('Password: ')
-
-		armaadmin.users.add(username, password, [], True)
+		setupUser()
 
 		print()
 		print('Installing...')
 
+		shutil.copy('config.py', 'armaadmin/')
 		install.run(self)
-
-		os.remove('armaadmin/users.db')
 		os.remove('armaadmin/config.py')
 
+		os.remove('armaadmin/users.db')
+
+		setupDirs()
+		setupInit()
+
+class upgrade(install):
+	def run(self):
 		print()
-		print('Making directories...')
+		print('Upgrading...')
 
-		import config
+		shutil.copy('config.py', 'armaadmin/')
+		install.run(self)
+		os.remove('armaadmin/config.py')
 
-		dir_util.mkpath(config.prefix)
-
-		if config.creation:
-			dir_util.copy_tree('config', config.config)
-
-		if config.api:
-			print()
-			print('Installing API...')
-			dir_util.copy_tree('api', config.api)
-
-		response = input('Which init system are you using: [1] SysV (Debian, Ubuntu, CentOS), [2] OpenRC (Gentoo), [3] Systemd (Arch, Fedora), [*] Other/None? ')
-
-		if response == "1":
-			print()
-			print('Installing SysV init script...')
-			file_util.copy_file('init/sysv/armaadmin', '/etc/init.d/')
-		elif response == "2":
-			print()
-			print('Installing OpenRC init script...')
-			file_util.copy_file('init/openrc/armaadmin', '/etc/init.d/')
-		elif response == "3":
-			print()
-			print('Installing Systemd init script...')
-			file_util.copy_file('init/systemd/armaadmin.service', '/usr/lib/systemd/system/')
-			subprocess.call(['systemctl', 'daemon-reload'])
+		setupDirs()
+		setupInit()
 
 setup(	name='ArmaAdmin',
 	version='2.0',
@@ -72,5 +91,5 @@ setup(	name='ArmaAdmin',
 	packages=[ 'armaadmin', 'armaadmin.routes' ],
 	package_data={ 'armaadmin': [ 'config.py', 'users.db', 'www/*.*', 'www/codemirror/*' ], 'armaadmin.routes': [ 'html/*' ] },
 	scripts=[ 'bin/armaadmin' ],
-	cmdclass={ 'install': post_install }
+	cmdclass={ 'install': post_install, 'upgrade': upgrade }
 )
