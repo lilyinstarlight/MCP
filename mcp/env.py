10c21648becef49162cd18640726cad6e32527ec
Foster McLane <fkmclane@gmail.com>
2016-06-27 11:38:32 -0400

    add script environment alongside server environment

--- mcp/env.py
+++ mcp/env.py
@@ -5,6 +5,7 @@ from mcp import config
 base_env = os.environ.copy()
 
 server_env = base_env.copy()
+script_env = base_env.copy()
 
 def get_env():
     return base_env
@@ -13,14 +14,6 @@ def get_server():
     return server_env
 
 def get_script():
-    script_env = get_server().copy()
-
-    if config.scripting:
-        if script_env.get('PYTHONPATH'):
-            script_env['PYTHONPATH'] += ':' + config.scripting
-        else:
-            script_env['PYTHONPATH'] = config.scripting
-
     return script_env
 
 def get_build(dst):
