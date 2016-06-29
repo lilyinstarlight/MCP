a53665f22d7f67577d3811f98ff146b5bbf45cd0
Foster McLane <fkmclane@gmail.com>
2016-06-29 15:42:09 -0400

    use new web.py page handler

--- mcp/page/routes.py
+++ mcp/page/routes.py
@@ -2,30 +2,34 @@ import os
 
 from mcp.lib import web
 
-class PageHandler(web.HTTPHandler):
-    page = 'index.html'
 
-    def do_get(self):
-        self.response.headers.set('Content-Type', 'text/html')
-        return 200, open(os.path.dirname(__file__) + '/html/' + self.page, 'r')
+class PageHandler(web.page.PageHandler):
+    directory = os.path.dirname(__file__) + '/html'
+
 
 class IndexHandler(PageHandler):
     page = 'index.html'
 
+
 class SetupHandler(PageHandler):
     page = 'setup.html'
+
 
 class ServerHandler(PageHandler):
     page = 'server.html'
 
+
 class AdminHandler(PageHandler):
     page = 'admin.html'
+
 
 class UserHandler(PageHandler):
     page = 'user.html'
 
+
 class LoginHandler(PageHandler):
     page = 'login.html'
+
 
 routes = {'/': IndexHandler, '/setup': SetupHandler, '/server': ServerHandler, '/admin': AdminHandler, '/user': UserHandler, '/login': LoginHandler}
 
