365f98cc951fbda4f6243d0a890cd655ad240d0a
Foster McLane <fkmclane@gmail.com>
2016-07-18 10:49:48 -0400

    use new log.py

--- mcp/common/log.py
+++ mcp/common/log.py
@@ -1,6 +1,4 @@
-import os
-import sys
-import time
+import log
 
 from mcp import config, cron
 
@@ -8,57 +6,6 @@ mcplog = None
 cmdlog = None
 httplog = None
 
-class Log(object):
-    def __init__(self, log):
-        if log:
-            os.makedirs(os.path.dirname(log), exist_ok=True)
-            self.log = open(log, 'a', 1)
-        else:
-            self.log = None
-
-    def timestamp(self):
-        return time.strftime('[%d/%b/%Y:%H:%M:%S %z]')
-
-    def write(self, string):
-        if self.log:
-            self.log.write(string)
-
-    def message(self, message):
-        self.write(self.timestamp() + ' ' + message + '\n')
-
-    def head(self, header):
-        self.message(header)
-        self.message('=' * len(header))
-
-    def info(self, message):
-        self.message('INFO: ' + message)
-
-    def warning(self, message):
-        self.message('WARNING: ' + message)
-
-    def error(self, message):
-        self.message('ERROR: ' + message)
-
-    def exception(self):
-        self.error('Caught exception:\n\t' + traceback.format_exc().replace('\n', '\n\t'))
-
-class HTTPLog(Log):
-    def __init__(self, log, access_log):
-        Log.__init__(self, log)
-
-        if access_log:
-            os.makedirs(os.path.dirname(access_log), exist_ok=True)
-            self.access_log = open(access_log, 'a', 1)
-        else:
-            self.access_log = None
-
-    def access_write(self, string):
-        if self.access_log:
-            self.access_log.write(string)
-
-    def request(self, host, request, code='-', size='-', rfc931='-', authuser='-'):
-        self.access_write(host + ' ' + rfc931 + ' ' + authuser + ' ' + self.timestamp() + ' "' + request + '" ' + code + ' ' + size + '\n')
-
-mcplog = Log(config.log)
-cmdlog = Log(config.cmdlog)
-httplog = HTTPLog(config.httpdlog, config.accesslog)
+mcplog = log.Log(config.log)
+cmdlog = log.Log(config.cmdlog)
+httplog = log.HTTPLog(config.httpdlog, config.accesslog)
