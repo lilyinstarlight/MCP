45c987e6553a861e63608b1595da701f7102f8ab
Foster McLane <fkmclane@gmail.com>
2018-06-25 08:45:26 -0500

    fix form id in admin interface

--- mcp/page/res/admin/script.js
+++ mcp/page/res/admin/script.js
@@ -616,7 +616,7 @@ var load = function() {
 		ev.preventDefault();
 	}, false);
 
-	document.getElementById('source_form').addEventListener('click', function(ev) {
+	document.getElementById('source_add_form').addEventListener('click', function(ev) {
 		submitSource();
 		change('sources', 'source_list');
 
